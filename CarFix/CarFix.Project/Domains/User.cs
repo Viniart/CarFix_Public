02241013ddc4350c226d882b34d5b31707bec1e0
Vinicio Oliveira Santos <37529671880@info.local>
2021-10-26 17:06:54 -0300

    Projeto Criado

---
+++ CarFix/CarFix.Project/Domains/User.cs
@@ -0,0 +1,26 @@
+﻿using CarFix.Project.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarFix.Project.Domains
+{
+    public class User : Entity
+    {
+        public User(string username, string email, string password, EnUserType userType, string phoneNumber)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            UserType = userType;
+            PhoneNumber = phoneNumber;
+        }
+
+        public string Username { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public EnUserType UserType { get; private set; }
+        public string PhoneNumber { get; private set; }
+    }
+}
---
+++ CarFix/CarFix.Project/Domains/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿]using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarFix.Project.Domains
+{
+    public class Vehicle : Entity
+    {
+        public Vehicle(string placa, string nomeModelo, string marca, int ano, string cor, int userId)
+        {
+            Placa = placa;
+            NomeModelo = nomeModelo;
+            Marca = marca;
+            Ano = ano;
+            Cor = cor;
+            UserId = userId;
+        }
+
+        public string Placa { get; private set; }
+        public string NomeModelo { get; private set; }
+        public string Marca { get; private set; }
+        public int Ano { get; private set; }
+        public string Cor { get; private set; }
+        public string ImagemVeiculo { get; private set; }
+
+        // Compositions
+        public int UserId { get; private set; }
+        public virtual User User { get; private set; }
+    }
+}
