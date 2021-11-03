7b342d26e5b0466700ef766ab8f0592fa91b9c1c
Vinicio Oliveira Santos <37529671880@info.local>
2021-11-03 17:08:52 -0300

    String de conexao adicionada

--- CarFix/CarFix.Project/Startup.cs
+++ CarFix/CarFix.Project/Startup.cs
@@ -4,11 +4,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +23,8 @@ namespace CarFix.Project
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CarFixContext>(options => options.UseSqlServer("Data Source=g19-database.cl3us3tplwnh.us-east-1.rds.amazonaws.com; Initial Catalog=CarFix;user=admin;pwd=Senai-132"));
+            var connectionString = ConfigurationManager.ConnectionStrings["LocalConnectionString"].ConnectionString;
+            services.AddDbContext<CarFixContext>(options => options.UseSqlServer(connectionString));
 
             services.AddControllers();
 
